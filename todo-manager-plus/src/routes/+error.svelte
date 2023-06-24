0eccca54026cc888eb2af5aabe108af9fc629a15
Lord-Lelsers <millan.kumar.24@bishops.com>
2023-06-24 00:07:07 -0700

    Error page

---
+++ todo-manager-plus/src/routes/+error.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { page } from '$app/stores';
+
+	import { goto } from '$app/navigation';
+
+	function backToHome(id) {
+		goto('/')
+	}
+</script>
+
+<header class="zeroBottomPadding">
+	<hgroup>
+		<h1 class="fiveLeftMargin">{$page.status}</h1>
+		<h2 class="fiveLeftMargin">{$page.error.message}</h2>
+	</hgroup>
+</header>
+
+<hr />
+
+<main>
+	<button class="nintyWidth zeroAutoMargin" on:click={backToHome}>
+		Return to dashboard
+	</button>
+
+</main>
